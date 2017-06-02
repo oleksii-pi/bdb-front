7e353e9aee611d1d33213d76c3e131989b2d71e5
AlekseyPi <tolexis@gmail.com>
2017-06-02 13:53:20 +0300

    Element id now editable

--- src/components/simpleblock/simpleblock-viewmodel.js
+++ src/components/simpleblock/simpleblock-viewmodel.js
@@ -4,12 +4,12 @@ var vmFactory = require('./../components').ViewModelFactory;
 module.exports = function (data) {
     var self = this;
 
-    self.id = ko.computed(() => data.id); // readonly
+    self.id = ko.observable(data.id);
     self.component = ko.computed(() => data.component); // readonly
-    self.x = ko.observable(data.x);
-    self.y = ko.observable(data.y);
-    self.width = ko.observable(data.width || 100);
-    self.height = ko.observable(data.height || 50);
+    self.x = ko.observable(data.x).extend({dataType: "integer"});;
+    self.y = ko.observable(data.y).extend({dataType: "integer"});;
+    self.width = ko.observable(data.width || 100).extend({dataType: "integer", range: {min: 100, max: 500}});
+    self.height = ko.observable(data.height || 50).extend({dataType: "integer", range: {min: 50, max: 500}});
     self.selected = ko.observable(false);
 
     self.hash = ko.computed(function(){
--- src/components/textbox/textbox-viewmodel.js
+++ src/components/textbox/textbox-viewmodel.js
@@ -4,11 +4,11 @@ var vmFactory = require('./../components').ViewModelFactory;
 module.exports = function (data) {
     var self = this;
 
-    self.id = ko.computed(() => data.id); // readonly
+    self.id = ko.observable(data.id);
     self.component = ko.computed(() => data.component); // readonly
     self.x = ko.observable(data.x).extend({dataType: "integer"});
     self.y = ko.observable(data.y).extend({dataType: "integer"});
-    self.width = ko.observable(data.width || 100).extend({dataType: "integer", range: {min: 50, max: 500}});
+    self.width = ko.observable(data.width || 100).extend({dataType: "integer", range: {min: 100, max: 500}});
     self.height = ko.observable(data.height || 50).extend({dataType: "integer", range: {min: 50, max: 500}});
     self.selected = ko.observable(false);
 
