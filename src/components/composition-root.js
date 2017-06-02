5519715c827475f3aedef05203ce9b2820775998
AlekseyPi <tolexis@gmail.com>
2017-06-02 12:20:33 +0300

    Some test initial data

--- src/components/composition-root.js
+++ src/components/composition-root.js
@@ -25,7 +25,7 @@ module.exports.run = function (svgParentNode) {
     //var diagramData = {component: 'diagram', id: 'diagram1'};
 
     //debug:
-    var diagramData = {component: 'diagram', id: 'diagram1', elements: [{component: 'textbox', id: 'textbox1', text: 'Some big welcome text. Hello world!', x: 100, y: 100, width: 200, height: 100}]};
+    var diagramData = {component: 'diagram', id: 'diagram1', elements: [{component: 'textbox', id: 'textbox1', text: 'Some big welcome text.\nHello world!\n1\n2', x: 100, y: 100, width: 200, height: 100}]};
 
     var diagramViewModel = components.ViewModelFactory(diagramData);
     var diagramView = components.ViewFactory(diagramViewModel, svgParentNode);
@@ -35,5 +35,5 @@ module.exports.run = function (svgParentNode) {
 
     diagramViewModel.elements()[0].commandSelect();
 
-    ko.applyBindings(diagramViewModel); //! should be in another place
+    ko.applyBindings(diagramViewModel); //! should be in another place ; consider: ko.applyBindings(diagramViewModel, paramsNode)
 };
