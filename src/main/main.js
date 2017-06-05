1d028f2772f9b9e2ece943154215fe8900d472e9
AlekseyPi <tolexis@gmail.com>
2017-06-05 13:22:14 +0300

    Fixed init

--- src/main/main.js
+++ src/main/main.js
@@ -5,14 +5,13 @@ require('./split.css');
 
 var split = require('split.js');
 var compositionRoot = require('./../components/composition-root');
-var params = require('./params/params');
+require('./params/params');
 
 $(document).ready(function() {
     split(['#diagram', '#params'], {
         sizes: [70, 30],
         minSize: 200
     });
-    params.init();
     compositionRoot.run($('#diagram')[0]);
 });
 
