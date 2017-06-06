69ff4b1c6b4e6245d7ea4b5850d25b2184676feb
AlekseyPi <tolexis@gmail.com>
2017-06-06 12:28:19 +0300

    Added mouse resize for block

--- src/components/block/block-view.js
+++ src/components/block/block-view.js
@@ -3,12 +3,20 @@ require('./block.css');
 var ko = require('knockout');
 
 const margin = 4;
+const resizeRectSize = 5;
 
 module.exports = function(vm, parentNode) {
     var g = d3.select(parentNode);
 
-    g.append('rect').attr("class", "element block");
-    g.append("foreignObject");
+    g.attr("class", "element block");
+
+    g.append('rect');
+    g.append('foreignObject');
+    g.append('rect')
+        .attr("class", "resizeRect")
+        .attr('width', resizeRectSize)
+        .attr('height', resizeRectSize)
+    ;
 
     g.select('foreignObject')
         .html(d =>
@@ -42,6 +50,17 @@ module.exports = function(vm, parentNode) {
             .attr('y', d => d.y() + margin)
             .attr('width', d => d.width() - margin * 2)
             .attr('height', d => d.height() - margin * 2)
+        ;
+
+        g.select('.resizeRect')
+            .attr('x', d => d.x() + d.width() - resizeRectSize)
+            .attr('y', d => d.y() + d.height() - resizeRectSize)
+            .attr('visibility', d => d.selected() ? 'visible' : 'hidden')
+            .call(d3.drag().on("drag", () => {
+                //vm.commandResize(d3.event.x - vm.x(), d3.event.y - vm.y());
+                vm.width(d3.event.x - vm.x());
+                vm.height(d3.event.y - vm.y());
+            }))
         ;
 
     };
