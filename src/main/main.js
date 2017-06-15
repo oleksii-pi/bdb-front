3d8576ed351238b644a8c80ae416e1a4f9157280
AlekseyPi <tolexis@gmail.com>
2017-06-15 21:10:48 +0300

    Show confirmation message before leaving the page

--- src/main/main.js
+++ src/main/main.js
@@ -7,6 +7,10 @@ var split = require('split.js');
 var compositionRoot = require('./../components/composition-root');
 require('./params/params');
 
+window.onbeforeunload = function() {
+    return "Are you sure you want to navigate away?";
+}
+
 $(document).ready(function() {
     split(['#diagram', '#params'], {
         sizes: [70, 30],
