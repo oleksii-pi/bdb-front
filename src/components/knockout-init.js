eb17f3f29e6176d4b36b424d47806c766c035346
AlekseyPi <tolexis@gmail.com>
2017-06-16 21:36:58 +0300

    CodeMirror binding handler moved to knockout-init

--- src/components/knockout-init.js
+++ src/components/knockout-init.js
@@ -1,3 +1,4 @@
+var CodeMirror = require('codemirror/lib/codemirror');
 var ko = require('knockout');
 
 module.exports = function() {
@@ -161,6 +162,34 @@ module.exports = function() {
     };
 
     //// bindingHandlers
+
+    // https://stackoverflow.com/a/17489451/3818473
+    ko.bindingHandlers.codemirror = {
+        init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
+            var options = valueAccessor();
+            var editor = CodeMirror.fromTextArea(element, options);
+
+            editor.on('change', function(cm) {
+                allBindingsAccessor().value(cm.getValue());
+            });
+
+            element.editor = editor;
+
+            if (allBindingsAccessor().value()) {
+                editor.setValue(allBindingsAccessor().value());
+            }
+            editor.refresh();
+
+            var wrapperElement = $(editor.getWrapperElement());
+            ko.utils.domNodeDisposal.addDisposeCallback(element, function () {
+                wrapperElement.remove();
+            });
+        },
+        update: function (element, valueAccessor) {
+            if(element.editor)
+                element.editor.refresh();
+        }
+    };
 
     ko.bindingHandlers.stringValue = {
         init: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
--- src/main/params/params.js
+++ src/main/params/params.js
@@ -1,40 +1,8 @@
-var CodeMirror = require('codemirror/lib/codemirror');
-
 require('codemirror/lib/codemirror.css');
 require('codemirror/mode/javascript/javascript');
 require('codemirror/addon/edit/matchbrackets');
 require('codemirror/addon/scroll/simplescrollbars');
 require('codemirror/addon/scroll/simplescrollbars.css');
-
-var ko = require('knockout');
-
 require('./params.css');
 require('./codemirror-theme.css');
 
-// https://stackoverflow.com/a/17489451/3818473
-ko.bindingHandlers.codemirror = {
-    init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
-        var options = valueAccessor();
-        var editor = CodeMirror.fromTextArea(element, options);
-
-        editor.on('change', function(cm) {
-            allBindingsAccessor().value(cm.getValue());
-        });
-
-        element.editor = editor;
-
-        if (allBindingsAccessor().value()) {
-            editor.setValue(allBindingsAccessor().value());
-        }
-        editor.refresh();
-
-        var wrapperElement = $(editor.getWrapperElement());
-        ko.utils.domNodeDisposal.addDisposeCallback(element, function () {
-            wrapperElement.remove();
-        });
-    },
-    update: function (element, valueAccessor) {
-        if(element.editor)
-            element.editor.refresh();
-    }
-};
